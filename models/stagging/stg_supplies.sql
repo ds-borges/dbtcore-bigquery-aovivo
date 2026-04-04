a625f0d120d611cbba1f66f98efac2bb413939ae
ds-borges <diego97sousa@gmail.com>
2026-04-04 08:25:55 -0300

    fix: call cents_to_dollars at stg supplies

--- models/stagging/stg_supplies.sql
+++ models/stagging/stg_supplies.sql
@@ -11,7 +11,6 @@ renamed as (
     select
 
         ----------  ids
-        -- Substituição manual do generate_surrogate_key
         id as supply_id,
         sku as product_id,
         name as supply_name,
@@ -19,11 +18,11 @@ renamed as (
         ---------- text
         perishable as is_perishable_supply,
 
+        ---------- identifiers
+        {{ dbt_utils.generate_surrogate_key(['id', 'sku']) }} as supply_uuid,
+
         ---------- numerics
-        -- Substituição manual do cents_to_dollars
-        {{ dbt_utils.generate_surrogate_key([['id', 'sku']]) }} as supply_uuid,
-        ---------- booleans
-        cents_to_dollars(cost) as supply_cost
+        {{ cents_to_dollars('cost') }} as supply_cost
 
     from source
 
