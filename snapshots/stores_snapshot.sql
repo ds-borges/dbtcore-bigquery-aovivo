ad0c4941187923cb9ec7726c6937664ed70c172f
ds-borges <diego97sousa@gmail.com>
2026-04-14 04:34:36 -0300

    Build: add snapshot function

---
+++ snapshots/stores_snapshot.sql
@@ -0,0 +1,15 @@
+{% snapshot stores_snapshot %}
+
+    {{
+        config(
+          target_schema='staging_snapshot',
+          unique_key='location_id',
+          strategy='check',
+          check_cols=['location_name', 'tax_rate']
+        )
+    }}
+
+    select *
+    from {{ ref('stg_locations') }}
+
+{% endsnapshot %}
